dce338292c42425a5929578ea9c4a80541d31cfa
Robson <Robson>
2017-02-12 21:17:35 -0200

    Creating core modules structures

---
+++ test/core/game.spec.js
@@ -0,0 +1,10 @@
+var chai = require('chai');
+var expect = chai.expect;
+
+var core = require('../../src/core/core');
+
+describe('Game', () => {
+  it('it should pass', () => {
+    expect(true).to.be.true;
+  });
+});
--- test/test.js
+++
@@ -1,8 +0,0 @@
-var chai = require('chai');
-var expect = chai.expect;
-
-describe('test', () => {
-  it('test', () => {
-    expect(true).to.be.true;
-  });
-});
