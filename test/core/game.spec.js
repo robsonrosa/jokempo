41feb075c29b818dbd3da4519ffc3b7aeb7a7e74
Robson <Robson>
2017-02-12 21:25:01 -0200

    This test shall not pass for Game

--- test/core/game.spec.js
+++ test/core/game.spec.js
@@ -4,7 +4,33 @@ var expect = chai.expect;
 var core = require('../../src/core/core');
 
 describe('Game', () => {
-  it('it should pass', () => {
-    expect(true).to.be.true;
+
+  describe('Deve ser possível criar um novo jogo', () => {
+    let name = 'nome de teste';
+    let description = 'descrição de teste';
+    let options = new core.GameOptionCollection();
+    let game = new core.Game(name, description, options);
+
+    it('Um jogo deve ser inicializado com um nome', () => {
+      expect(game.getName()).to.be.equals(name);
+    });
+
+    it('Um jogo deve ser inicializado com uma descrição', () => {
+      expect(game.getDescription()).to.be.equals(description);
+    });
+
+    it('Um jogo deve ser inicializado com uma coleção de opções', () => {
+      expect(game.getOptions()).to.be.equals(options);
+    });
+
+    it('Um jogo não deve expor suas propriedades', () => {
+      expect(game.name).to.be.undefined;
+      expect(game.description).to.be.undefined;
+      expect(game.options).to.be.undefined;
+      expect(game.setName).to.be.undefined;
+      expect(game.setDescription).to.be.undefined;
+      expect(game.setOptions).to.be.undefined;
+    });
   });
+
 });
