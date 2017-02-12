370697e455c98255ff82a1a6081c830002c3667b
Robson <Robson>
2017-02-12 21:28:22 -0200

    Creating the Game class

--- src/core/core.js
+++ src/core/core.js
@@ -1,3 +1,11 @@
 module.exports = {
+  GameOptionCollection: class {},
 
+  Game: class {
+    constructor(name, description, options) {
+      this.getName = () => name;
+      this.getDescription = () => description;
+      this.getOptions = () => options;
+    }
+  }
 };
