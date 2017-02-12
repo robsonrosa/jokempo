53442293b2a887674b11f6604fd5cd17de7578a3
Robson <Robson>
2017-02-12 20:23:18 -0200

    Adding gulp watch task for testing

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+// from gulp/recipes
+// https://github.com/gulpjs/gulp/blob/master/docs/recipes/mocha-test-runner-with-gulp.md
+
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+var gutil = require('gulp-util');
+
+gulp.task('default', function () {
+  gulp.watch(['src/**', 'test/**'], ['mocha']);
+});
+
+gulp.task('mocha', function () {
+  return gulp.src(['src/core/**/*.js', 'test/**/*.js'], { read: false })
+    .pipe(mocha())
+    .on('error', gutil.log);
+});
---
+++ test/test.js
@@ -0,0 +1,8 @@
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('test', () => {
+  it('test', () => {
+    expect(true).to.be.true;     
+  });
+});
