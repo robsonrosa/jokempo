1f34bc4dbb6a80a67fd444f467c74919531ae643
Robson <Robson>
2017-02-14 04:12:59 -0200

    Implementing basic BotPlayer

--- src/core/bot.js
+++ src/core/bot.js
@@ -1,3 +1,12 @@
-module.exports = {
+let core = require('./core');
 
+module.exports = {
+  BotPlayer: class {
+    constructor(collection, name) {
+      let choose = () => collection.get(Math.floor(Math.random() * collection.size()));
+
+      this.getName = () => name;
+      this.play = () => new core.PlayerOption(name, choose());
+    }
+  }
 };
