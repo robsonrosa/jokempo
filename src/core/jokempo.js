5b85ea39d8d6d128efff0634535e986c3f232608
Robson <Robson>
2017-02-14 23:18:09 -0200

    Adding Jokenpo helper class and its tests

---
+++ src/core/jokempo.js
@@ -0,0 +1,109 @@
+let { BotPlayer } = require('./bot');
+let { GameMatch, PlayerOption } = require('./core');
+let { GameFactory } = require('./factory');
+let { GameValidator } = require('./validation');
+
+class GamePlayer {
+  constructor(instance) {
+    this.get = () => ({ play: () => new GameMatch(instance.game, instance.players).result() });
+  }
+}
+
+class Player {
+  constructor(instance) {
+    let findChoice = (choice) => instance.game.getOptions().get(choice);
+    this.get = (player) => new PlayerOption(player.name, findChoice(player.choice));
+  }
+}
+
+class WithBots {
+  constructor(instance) {
+    let createBot = () => new BotPlayer(instance.game.getOptions(), 'bot' + instance.botId()).play();
+    let addBots = (botsCount) => {
+      for (let i = 0; i < botsCount; i++) {
+        instance.players.push(createBot());
+      }
+    };
+
+    this.get = (botsCount) => {
+      addBots(botsCount);
+      return {
+        bots: () => new GamePlayer(instance).get()
+      };
+    };
+  }
+}
+
+class Multiplayer {
+  constructor(instance) {
+    this.get = (players) => {
+      instance.reset();
+      players.forEach(p => instance.players.push(new Player(instance).get(p)));
+      return new GamePlayer(instance).get();
+    };
+  }
+}
+
+class Duel {
+  constructor(instance) {
+    this.get = () => {
+      instance.reset();
+      return {
+        between: (challenger) => {
+          instance.players.push(new Player(instance).get(challenger));
+          return {
+            and: (opponent) => {
+              instance.players.push(new Player(instance).get(opponent));
+              return new GamePlayer(instance).get();
+            }
+          };
+        }
+      };
+    };
+  }
+}
+
+class Alone {
+  constructor(instance) {
+    this.get = (player) => {
+      instance.reset();
+      instance.players.push(new Player(instance).get(player));
+      return {
+        against: (botsCount) => new WithBots(instance).get(botsCount || 1)
+      };
+    };
+  }
+}
+
+class Watch {
+  constructor(instance) {
+    this.get = (botsCount) => {
+      instance.reset();
+      return new WithBots(instance).get(botsCount || 2);
+    };
+  }
+}
+
+class Jokempo {
+  constructor(settings) {
+    let botId = 0;
+    let players = [];
+    let game = new GameFactory(new GameValidator()).create(settings);
+
+    let instance = {
+      game: game,
+      players: players,
+      botId: () => ++botId,
+      reset: () => players.splice(0),
+    };
+
+    this.watch = (botsCount) => new Watch(instance).get(botsCount);
+    this.alone = (player) => new Alone(instance).get(player);
+    this.duel = () => new Duel(instance).get();
+    this.multiplayer = (players) => new Multiplayer(instance).get(players);
+  }
+}
+
+module.exports = {
+  Jokempo: Jokempo
+};
---
+++ test/core/integrated.spec.js
@@ -0,0 +1,115 @@
+let chai = require('chai');
+let expect = chai.expect;
+
+let { Jokempo } = require('../../src/core/jokempo');
+let settings = require('../resources/settings.json');
+
+let options = ['rock', 'paper', 'scissor'];
+let results = ['Vitória', 'Empate'];
+let includesAtLeastOne = arr => (s => arr.some(o => s.includes(o)));
+
+let challenger = { name: 'challenger', choice: 'rock' };
+let opponent = { name: 'opponent', choice: 'paper' };
+let foreigner = { name: 'foreigner', choice: 'scissor' };
+
+describe('Integrated Tests', () => {
+  describe('Quando eu assistir uma partida', () => {
+    describe('E não especificar a quantidade de bots', () => {
+      let watch = new Jokempo(settings).watch().bots().play();
+
+      it('Então deverá ter um resultado válido', () => {
+        let text = watch.toString();
+        expect(text).to.includes('bot1');
+        expect(text).to.includes('bot2');
+        expect(text).to.satisfy(includesAtLeastOne(options));
+        expect(text).to.satisfy(includesAtLeastOne(results));
+      });
+    });
+
+    describe('E especificar 4 bots', () => {
+      let watch = new Jokempo(settings).watch(4).bots().play();
+
+      it('Então deverá ter 6 resultados válidos', () => {
+        expect(watch).to.have.length(6);
+        expect(watch[0].toString()).to.includes('bot1');
+        expect(watch[0].toString()).to.includes('bot2');
+        expect(watch[1].toString()).to.includes('bot1');
+        expect(watch[1].toString()).to.includes('bot3');
+        expect(watch[2].toString()).to.includes('bot1');
+        expect(watch[2].toString()).to.includes('bot4');
+        expect(watch[3].toString()).to.includes('bot2');
+        expect(watch[3].toString()).to.includes('bot3');
+        expect(watch[4].toString()).to.includes('bot2');
+        expect(watch[4].toString()).to.includes('bot4');
+        expect(watch[5].toString()).to.includes('bot3');
+        expect(watch[5].toString()).to.includes('bot4');
+        watch.forEach(r => {
+          expect(r.toString()).to.satisfy(includesAtLeastOne(options));
+          expect(r.toString()).to.satisfy(includesAtLeastOne(results));
+        });
+      });
+    });
+  });
+
+  describe('Quando eu jogar sozinho', () => {
+    describe('E não especificar a quantidade de bots', () => {
+      let alone = new Jokempo(settings).alone(challenger).against().bots().play();
+
+      it('Então deverá ter um resultado válido', () => {
+        let text = alone.toString();
+        expect(text).to.includes('challenger');
+        expect(text).to.includes('bot1');
+        expect(text).to.satisfy(includesAtLeastOne(options));
+        expect(text).to.satisfy(includesAtLeastOne(results));
+      });
+    });
+
+    describe('E especificar 2 bots', () => {
+      let alone = new Jokempo(settings).alone(challenger).against(2).bots().play();
+
+      it('Então deverá ter 3 resultados válidos', () => {
+        expect(alone).to.have.length(3);
+        expect(alone[0].toString()).to.includes('challenger');
+        expect(alone[0].toString()).to.includes('bot1');
+        expect(alone[1].toString()).to.includes('challenger');
+        expect(alone[1].toString()).to.includes('bot2');
+        expect(alone[2].toString()).to.includes('bot1');
+        expect(alone[2].toString()).to.includes('bot2');
+        alone.forEach(r => {
+          expect(r.toString()).to.satisfy(includesAtLeastOne(options));
+          expect(r.toString()).to.satisfy(includesAtLeastOne(results));
+        });
+      });
+    });
+  });
+
+  describe('Quando eu jogar contra outro jogador', () => {
+    let duel = new Jokempo(settings).duel().between(challenger).and(opponent).play();
+
+    it('Então deverá ter um resultado válido', () => {
+      let text = duel.toString();
+      expect(text).to.includes('challenger');
+      expect(text).to.includes('opponent');
+      expect(text).to.satisfy(includesAtLeastOne(options));
+      expect(text).to.satisfy(includesAtLeastOne(results));
+    });
+  });
+
+  describe('Quando eu jogar de galera', () => {
+    let multiplayer = new Jokempo(settings).multiplayer([challenger, opponent, foreigner]).play();
+
+    it('Então deverá ter 3 resultados válidos', () => {
+      expect(multiplayer).to.have.length(3);
+      expect(multiplayer[0].toString()).to.includes('challenger');
+      expect(multiplayer[0].toString()).to.includes('opponent');
+      expect(multiplayer[1].toString()).to.includes('challenger');
+      expect(multiplayer[1].toString()).to.includes('foreigner');
+      expect(multiplayer[2].toString()).to.includes('opponent');
+      expect(multiplayer[2].toString()).to.includes('foreigner');
+      multiplayer.forEach(r => {
+        expect(r.toString()).to.satisfy(includesAtLeastOne(options));
+        expect(r.toString()).to.satisfy(includesAtLeastOne(results));
+      });
+    });
+  });
+});
